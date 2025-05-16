c629d5e091460c3eacf2aac5afe9cd0c7fd576f2
AFKMartin <javiermarace@protonmail.com>
2025-05-16 06:56:07 +0100

    36 commented

--- 36. Valid Sudoku.py
+++ 36. Valid Sudoku.py
@@ -22,29 +22,31 @@ from typing import List
 # --- My solution
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        rows = [set() for _ in range(9)]
-        cols = [set() for _ in range(9)]
-        boxes = {}
+        rows = [set() for _ in range(9)] # create a list of sets for storing rows
+        cols = [set() for _ in range(9)] # same for cols
+        boxes = {} # creates an empty dictionary "That's a Surprise Tool That Can Help us Later"
 
-        for r in range(9):
-            for c in range(9):
-                val = board[r][c]
-                if val == ".":
-                    continue
+        for r in range(9): # loop 9 times, if needed based on value of r
+            for c in range(9): # loop 9 times, if needed based on value of c
+                val = board[r][c] # val takes each cell value from the board as we iterate rows and columns.
+                if val == ".": # checks if value is "."
+                    continue # if it is go back to the second loop
                 
-                if val in rows[r]:
+                if val in rows[r]: # If val exists in rows, return false, the sudoku is not valid
                     return False
-                if val in cols[c]:
+                if val in cols[c]: # same for cols, they work literally the same
                     return False
-                if val in boxes.get((r//3, c//3), set()):
-                    return False
-                # Rookie mistake...
-                rows[r].add(val)
-                cols[c].add(val)
-                if (r//3, c//3) not in boxes:
+                if val in boxes.get((r//3, c//3), set()): # this creates in boxes a total of 9 sets, if needed, and is checking if val value... yeah... is in the current one
+                    return False 
+                # Rookie mistake... I forgot this part
+                rows[r].add(val) # add the value to rows, otherwise it will always be true, yeah I forgot 
+                cols[c].add(val) # add the value to cols
+                if (r//3, c//3) not in boxes: # this all just to add the value to the set in the box, maybe there is a better way, my head hurt
                     boxes[(r//3, c//3)] = set()
                 boxes[(r//3, c//3)].add(val)
         return True
 # --- Test 
 sol = Solution()
 print(sol.isValidSudoku(board))
+
+# I had to comment this code since I was losing my mind somehow, whatever, it works and I thinkg is good enough, I'm done wuith this

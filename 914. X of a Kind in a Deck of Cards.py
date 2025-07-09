43b3467f752a3ec4ae6c7ac75ac535bf1d654a89
AFKMartin <javiermarace@protonmail.com>
2025-07-09 20:08:40 +0100

    https://en.meming.world/images/en/b/be/But_It%27s_Honest_Work.jpg

---
+++ 914. X of a Kind in a Deck of Cards.py
@@ -0,0 +1,39 @@
+# https://leetcode.com/problems/x-of-a-kind-in-a-deck-of-cards/description/
+# You are given an integer array deck where deck[i] represents the number written on the ith card.
+# Partition the cards into one or more groups such that:
+# Each group has exactly x cards where x > 1, and
+# All the cards in one group have the same integer written on them.
+# Return true if such partition is possible, or false otherwise.
+
+# Example 1:
+
+# Input: deck = [1,2,3,4,4,3,2,1]
+# Output: true
+# Explanation: Possible partition [1,1],[2,2],[3,3],[4,4].
+deck = [1,1,1,2,2,2,2,2,3,3,3,3,3]
+# --- My solution
+class Solution:
+    def hasGroupsSizeX(self, deck) -> bool:
+        c = {}
+        for card in deck:
+            if card in c:
+                c[card] +=1
+            else:
+                c[card] = 1
+        c = list(c.values())
+
+        def gcd(a,b):
+            while b:
+                a , b = b ,a % b
+            return a
+
+        size = c[0]
+        for num in c[1:]:
+            size = gcd(size, num)
+
+        return size > 1
+
+
+# --- Test
+sol = Solution()
+print(sol.hasGroupsSizeX(deck))

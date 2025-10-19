ecc31cd8497fe5ddf34d926c9bb33ad445a75660
AFKMartin <javiermarace@protonmail.com>
2025-10-19 19:06:12 +0100

    19 10

---
+++ 251 - 500/349. Intersection of Two Arrays.py
@@ -0,0 +1,16 @@
+# https://leetcode.com/problems/intersection-of-two-arrays/description/
+# Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the result must be unique and you may return the result in any order.
+
+# Example 1:
+
+# Input: nums1 = [1,2,2,1], nums2 = [2,2]
+# Output: [2]
+nums1 = [1,2,2,1]
+nums2 = [2,2]
+# --- My solution
+class Solution:
+    def intersection(self, nums1, nums2):
+        return list(set(nums1) & set(nums2))
+# --- Test
+sol = Solution()
+print(sol.intersection(nums1, nums2))

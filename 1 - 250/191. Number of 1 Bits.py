320ec89481d991cab4b613e78c709901a40a4709
AFKMartin <javiermarace@protonmail.com>
2026-02-24 22:56:45 +0000

    191

---
+++ 1 - 250/191. Number of 1 Bits.py
@@ -0,0 +1,21 @@
+# https://leetcode.com/problems/number-of-1-bits/
+# Given a positive integer n, write a function that returns the number of set bits in its binary representation (also known as the Hamming weight).
+
+# Example 1:
+# Input: n = 11
+# Output: 3
+# Explanation:
+# The input binary string 1011 has a total of three set bits.
+n = 11
+# --- My solution
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        count = 0
+        while n:
+            count += n & 1
+            n >>= 1
+        return count
+
+# --- Test
+sol = Solution() 
+print(sol.hammingWeight(n))

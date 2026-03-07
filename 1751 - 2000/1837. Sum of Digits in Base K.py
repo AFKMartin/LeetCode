3d964b22b704ef0a7c4df0e2f27b6980a928aef2
AFKMartin <javiermarace@protonmail.com>
2026-03-07 14:40:36 +0000

    1837

---
+++ 1751 - 2000/1837. Sum of Digits in Base K.py
@@ -0,0 +1,22 @@
+# https://leetcode.com/problems/sum-of-digits-in-base-k/description
+# Given an integer n (in base 10) and a base k, return the sum of the digits of n after converting n from base 10 to base k.
+# After converting, each digit should be interpreted as a base 10 number, and the sum should be returned in base 10.
+
+# Example 1:
+
+# Input: n = 34, k = 6
+# Output: 9
+# Explanation: 34 (base 10) expressed in base 6 is 54. 5 + 4 = 9.
+n = 34
+k = 6
+# --- My solution
+class Solution:
+    def sumBase(self, n, k):
+        c = 0
+        while n > 0:
+            c += n % k
+            n //= k
+        return c
+# --- Test
+sol = Solution()
+print(sol.sumBase(n, k))

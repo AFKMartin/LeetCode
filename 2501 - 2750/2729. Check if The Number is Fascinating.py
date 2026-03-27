68b5726663fa93a8aa9081821d551fa065e8710d
AFKMartin <javiermarace@protonmail.com>
2026-03-27 22:57:49 +0000

    2729

---
+++ 2501 - 2750/2729. Check if The Number is Fascinating.py
@@ -0,0 +1,21 @@
+# https://leetcode.com/problems/check-if-the-number-is-fascinating
+# You are given an integer n that consists of exactly 3 digits.
+# We call the number n fascinating if, after the following modification, the resulting number contains all the digits from 1 to 9 exactly once and does not contain any 0's:
+# Concatenate n with the numbers 2 * n and 3 * n.
+# Return true if n is fascinating, or false otherwise.
+# Concatenating two numbers means joining them together. For example, the concatenation of 121 and 371 is 121371.
+
+# Example 1:
+
+# Input: n = 192
+# Output: true
+# Explanation: We concatenate the numbers n = 192 and 2 * n = 384 and 3 * n = 576. The resulting number is 192384576. This number contains all the digits from 1 to 9 exactly once.
+n = 100
+# --- My solution
+class Solution:
+    def isFascinating(self, n: int) -> bool:
+        s = str(n) + str(2*n) + str(3*n)
+        return len(s) == 9 and set(s) == set("123456789")
+# --- Test
+sol = Solution()
+print(sol.isFascinating(n))
